53d640c253fe8a6da3d1f4db488f4e1e2270d6a4
AYUSH PRABHU <ayushprabhu20@gmail.com>
2025-02-14 11:32:21 +0000

    temp

--- my-hardhat-project/hardhat.config.js
+++ my-hardhat-project/hardhat.config.js
@@ -5,48 +5,299 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version:"0.8.28",
+        version: "0.5.10",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.25",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.0",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.12",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.8",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.11",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
       },
       {
         version: "0.5.5",
-      },
-      {
-        version: "0.6.7",
-        settings: {},
-      },
-      {
-        version: "0.8.17",
         settings: {
           optimizer: {
-            enabled: true,
-            runs: 200,
+            enabled: false,
+            runs: 1,
           },
         },
       },
       {
-        version: "0.7.6",
+        version: "0.4.24",
         settings: {
           optimizer: {
-            enabled: true,
-            runs: 200,
+            enabled: false,
+            runs: 1,
           },
         },
       },
       {
-        version: "0.6.12",
+        version: "0.4.6",
         settings: {
           optimizer: {
-            enabled: true,
-            runs: 200,
+            enabled: false,
+            runs: 1,
           },
         },
       },
       {
-        version: "0.5.17",
+        version: "0.5.0",
         settings: {
           optimizer: {
-            enabled: true,
-            runs: 200,
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.21",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.12",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.13",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.24",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.18",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.23",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.14",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.4",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.7",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.7",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.18",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.9",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.8",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.3",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.14",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.2",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.7",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.23",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.11",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.0",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.11",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.9",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.15",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
           },
         },
       },
@@ -54,12 +305,703 @@ module.exports = {
         version: "0.4.26",
         settings: {
           optimizer: {
-            enabled: true,
-            runs: 200,
+            enabled: false,
+            runs: 1,
           },
         },
-      }
-      
+      },
+      {
+        version: "0.4.8",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.7",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.10",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.6",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.16",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.2",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.3",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.0",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.19",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.17",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.1",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.8",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.0",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.25",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.10",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.10",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.10",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.21",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.24",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.18",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.6",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.25",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.99",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.0",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.4",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.3",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.22",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.2",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.17",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.20",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.0",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.3.5",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.14",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.8",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.16",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.10",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.22",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.4",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.19",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.20",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.22",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.15",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.22",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.4",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.21",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.23",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.2",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.5",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.25",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.3",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.3.6",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.1",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.4",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.4",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.23",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.20",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.1",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.1",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.13",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.18",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.1",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.12",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.9",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.1",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.13",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.17",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.6",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.9",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.26",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.0",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.13",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.5",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.19",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.15",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.5.8",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
+      {
+        version: "0.4.26",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 1,
+          },
+        },
+      },
     ],
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true, // Important for large codebases
+    },
+  },
+  mocha: {
+    timeout: 200000, // Increase timeout for long compilations
   },
 };
